dee9024bde2542b0134eab1906b55334586e5143
bruce gordon <bruce.gordon@centurylink.com>
2020-03-06 11:35:53 +0000

    initial cut

---
+++ vars/addCaCert.groovy
@@ -0,0 +1,59 @@
+import groovy.json.JsonSlurperClassic
+import groovy.json.JsonOutput;
+
+#!/usr/bin/env groovy
+
+
+@NonCPS
+def jsonParse(def json) {
+    new groovy.json.JsonSlurperClassic().parseText(json)
+}
+
+def call(auth,serviceName,location,org,namespace,cluster,findrZone, findrUsername, findrPassword,zoneName) {
+
+    //initial authentication
+    def response = httpRequest httpMode: 'POST',
+            url: 'https://portr.ctl.io/portr/authenticate',
+            customHeaders:[[name:'Authorization', value:"Basic ${auth}"]]
+    def json = jsonParse(response.content)
+    def portrToken = "${json.token}"
+
+    //request to get the cert
+    response = httpRequest httpMode: 'GET',
+            contentType: 'APPLICATION_JSON',
+            validResponseCodes: '200:405',
+            url: 'https://fastr.ctl.io/api/orgs/sso/mastrapplication/' + serviceName,
+            customHeaders:[[name:'Authorization', value:"Bearer ${portrToken}"]]
+
+    if (response.status == 404) {
+
+        def payload =" { " +
+                " \"metadata\": {                                                   " +
+                "    \"location\": \"" + location + "\",                            " +
+                "    \"organization\": \"" + org + "\",                             " +
+                "    \"resource\": \"mastrapplication\",                            " +
+                "    \"name\": \"" + serviceName + "\"                              " +
+                " },                                                                " +
+                " \"spec\": {                                                       " +
+                "   \"type\": \"ca-certificate\",                                   " +
+                "    \"cluster\": \"" + cluster + "\",                              " +
+                "    \"namespace\": \"" + namespace + "\",                          " +
+                "    \"renewalDays\": 30,                                           " +
+                "    \"findrZone\":  \"" + findrZone + "\",                         " +
+                "    \"findrZoneName\":\"" + zoneName + "\",                   " +
+                "    \"findrUsername\": \"" + findrUsername + "\",                  " +
+                "    \"subject\": \"" + serviceName + "." + zoneName + "\",       " +
+                "    \"findrPassword\": \"" + findrPassword + "\",                  " +
+                "    \"findrRecordName\": \"" + serviceName + "\"                    " +
+                "}                                                                  " +
+                "}"
+
+        //request to get the records
+        response = httpRequest httpMode: 'POST',
+                contentType: 'APPLICATION_JSON',
+                requestBody: payload,
+                url: 'https://fastr.ctl.io/api/orgs/sso/mastrapplications',
+                customHeaders:[[name:'Authorization', value:"Bearer ${portrToken}"]]
+    }
+
+}
